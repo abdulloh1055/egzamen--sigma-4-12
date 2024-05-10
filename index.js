4596369ce628827fa2ffabffc4cb047e8351cb35
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-05-10 20:07:24 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,66 @@
+// 1 - sovol
+// alert(5 % 2);
+
+// 2- sovol
+// let son  = (Math.random() *10);
+// alert(taxminiyson)
+
+// 3- vazifa
+// let taxminiyson = Math.floor (Math.random () *12.510)
+// let a  = '12';
+// let b  = Number(a)
+
+// alert(b)
+
+// 4 - vazifa
+// let soz = "mars it school";
+// function harf() {
+//     alert(soz.length);
+// } 
+
+// harf();
+
+// 5 -vazifa
+// let ismlar = "mars it school";
+
+// for(let i = 0 < 10; i ++)
+//  console.log(ismlar)
+
+// 6-vazifa
+//  let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ];
+// let ism = harflar[0] + harflar[1] + harflar[2] + harflar[21] + harflar[11] + harflar[11] + harflar[13] + harflar[6] 
+
+// console.log(ism);
+
+// 7 - vazifa
+// let yosh = Number(prompt("Yoshingizni"));
+
+// if (yosh > 18) {
+//     alert();
+// } else {
+//     alert ("balovatga yetmagan")
+// }
+
+// 8-savol String metodlari vs Array metodlari
+
+// let ism = prompt("Ismingizni kiriting:");
+//  let teskari = "";
+
+//  for (var i = ism.length - 1; i >= 0; i--) {
+//      teskari += ism[i];
+//  } 
+
+//  console.log(teskari);
+
+// 10-savol Array metodlari
+//  let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//  console.log("juftli sonlar:");
+
+//  for (let i = 0; i < sonlar.length; i ++) {
+//      if (sonlar[i] % 2 === 0) {
+//          console.log(sonlar[i]);
+//      }
+//  }
+
+
